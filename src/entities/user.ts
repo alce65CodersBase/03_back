174e956d4e4ad59aa41e6881d4fd430c3e30877d
Alejandro Cerezo <alce65@hotmail.es>
2023-03-26 15:05:47 +0200

    ✨ Update entities with joi schemas

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,3 +1,4 @@
+import { Joi, ValidationError } from 'express-validation';
 import { ImageInfo } from '../interfaces/image';
 
 export type Role = 'admin' | 'user';
@@ -11,3 +12,26 @@ export type User = {
   role: string;
   image: ImageInfo;
 };
+
+export const registerUserJoiSchema = Joi.object().keys({
+  email: Joi.string().email().required().messages({
+    'string.base': `"email" debe ser tipo 'texto'`,
+    'string.email': `El "email"  no es válido`,
+    'string.empty': `El "email" no puede faltar`,
+  }),
+
+  passwd: Joi.string()
+    .regex(/[a-zA-Z0-9]{5,16}/)
+    .required(),
+  firstName: Joi.string().required(),
+  surname: Joi.string().required(),
+  // Server generated role: Joi.string().valid('user', 'admin'),
+  // Server generated: image: ImageInfo;
+});
+
+export const loginJoiSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .regex(/[a-zA-Z0-9]{3,30}/)
+    .required(),
+});
