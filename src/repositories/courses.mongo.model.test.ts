7546261a94b8c131753cef08bbf7da969dbd5604
Alejandro Cerezo <alce65@hotmail.es>
2023-03-28 00:01:49 +0200

    ♻️ Update tests using Mongoose exec

--- src/repositories/courses.mongo.model.test.ts
+++ src/repositories/courses.mongo.model.test.ts
@@ -24,7 +24,7 @@ describe('Given Courses Mongoose Schema and Model', () => {
       }
     });
     afterEach(async () => {
-      await CourseModel.deleteMany();
+      await CourseModel.deleteMany().exec();
       mongoose.disconnect();
     });
   });
